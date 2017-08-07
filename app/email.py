b1605f5b5389c670ef8bfbc1c2866001104c4f48
Connor Finley <connor.finley5@gmail.com>
2017-08-06 21:51:12 -0400

    Remove async from email.py

--- app/email.py
+++ app/email.py
@@ -1,12 +1,6 @@
-from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
 
 
 def send_email(to, subject, template, **kwargs):
@@ -15,6 +9,5 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    with app.app_context():
+        mail.send(msg)
