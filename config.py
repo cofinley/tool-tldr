ebf4479629dba4d522a8e493d57787fed0a2a3d8
Connor Finley <connor.finley5@gmail.com>
2017-08-06 21:20:44 -0400

    Remove ssl flag for flask-mail

--- config.py
+++ config.py
@@ -22,7 +22,6 @@ class Config:
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USE_SSL = False
 	MAIL_SUBJECT_PREFIX = '[Tool TL;DR]'
 
 	ADMIN = os.environ.get('TOOL_TLDR_ADMIN')
