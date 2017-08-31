84b5ea7f588c95fa0081c73bc45efac6044992c6
Connor Finley <connor.finley5@gmail.com>
2017-08-31 17:31:58 -0400

    Add tool format info to about page.
    Closes #50

--- config.py
+++ config.py
@@ -42,6 +42,7 @@ class DevelopmentConfig(Config):
 	DEBUG = True
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 	'sqlite:///' + os.path.join(basedir, 'app-dev.db')
+	TEMPLATES_AUTO_RELOAD = True
 
 
 class TestingConfig(Config):
