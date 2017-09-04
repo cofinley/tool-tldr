85d0248b94e7000e92f6102f510028c5d8cda19d
Connor Finley <connor.finley5@gmail.com>
2017-09-04 13:28:36 -0400

    Restart alembic migrations

--- migrations/env.py
+++ migrations/env.py
@@ -18,9 +18,11 @@ logger = logging.getLogger('alembic.env')
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
+
 config.set_main_option('sqlalchemy.url',
-                       current_app.config.get('SQLALCHEMY_DATABASE_URI'))
+					   current_app.config.get('SQLALCHEMY_DATABASE_URI'))
 target_metadata = current_app.extensions['migrate'].db.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -29,7 +31,7 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
+	"""Run migrations in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -40,49 +42,50 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+	url = config.get_main_option("sqlalchemy.url")
+	context.configure(url=url)
 
-    with context.begin_transaction():
-        context.run_migrations()
+	with context.begin_transaction():
+		context.run_migrations()
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
+	"""Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
 
-    # this callback is used to prevent an auto-migration from being generated
-    # when there are no changes to the schema
-    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
-    def process_revision_directives(context, revision, directives):
-        if getattr(config.cmd_opts, 'autogenerate', False):
-            script = directives[0]
-            if script.upgrade_ops.is_empty():
-                directives[:] = []
-                logger.info('No changes in schema detected.')
+	# this callback is used to prevent an auto-migration from being generated
+	# when there are no changes to the schema
+	# reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
+	def process_revision_directives(context, revision, directives):
+		if getattr(config.cmd_opts, 'autogenerate', False):
+			script = directives[0]
+			if script.upgrade_ops.is_empty():
+				directives[:] = []
+				logger.info('No changes in schema detected.')
 
-    engine = engine_from_config(config.get_section(config.config_ini_section),
-                                prefix='sqlalchemy.',
-                                poolclass=pool.NullPool)
+	engine = engine_from_config(config.get_section(config.config_ini_section),
+								prefix='sqlalchemy.',
+								poolclass=pool.NullPool)
 
-    connection = engine.connect()
-    context.configure(connection=connection,
-                      target_metadata=target_metadata,
-                      compare_type=True,
-                      process_revision_directives=process_revision_directives,
-                      **current_app.extensions['migrate'].configure_args)
+	connection = engine.connect()
+	context.configure(connection=connection,
+					  target_metadata=target_metadata,
+					  compare_type=True,
+					  process_revision_directives=process_revision_directives,
+					  **current_app.extensions['migrate'].configure_args)
 
-    try:
-        with context.begin_transaction():
-            context.run_migrations()
-    finally:
-        connection.close()
+	try:
+		with context.begin_transaction():
+			context.run_migrations()
+	finally:
+		connection.close()
+
 
 if context.is_offline_mode():
-    run_migrations_offline()
+	run_migrations_offline()
 else:
-    run_migrations_online()
+	run_migrations_online()
