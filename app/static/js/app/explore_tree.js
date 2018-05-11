b96a6a6af8c8cb02943b0c669c9ccdf6deb5ce5a
Connor Finley <connor.finley5@gmail.com>
2018-05-11 19:05:30 -0400

    Fix issue where explore tree wasn't loading

--- app/static/js/app/explore_tree.js
+++ app/static/js/app/explore_tree.js
@@ -1,51 +1,64 @@
-var generalTreeSelector = ".explore-tree";
-var exploreTreeSelector = ".explore-tree-explore";
-var editTreeSelector = ".explore-tree-edit";
+var explore = (function () {
 
-var treeOptions = {
-    dragAndDrop: false,
-    openedIcon: $('<span class="opened-icon toggler"></span>'),
-    closedIcon: $('<span class="closed-icon toggler"></span>'),
-    useContextMenu: false,
-    autoEscape: false,
-    selectable: false
-};
+    var generalTreeSelector = ".explore-tree";
+    var exploreTreeSelector = ".explore-tree-explore";
+    var editTreeSelector = ".explore-tree-edit";
 
-$(exploreTreeSelector).tree(treeOptions);
+    var treeOptions = {
+        dragAndDrop: false,
+        openedIcon: $('<span class="opened-icon toggler"></span>'),
+        closedIcon: $('<span class="closed-icon toggler"></span>'),
+        useContextMenu: false,
+        autoEscape: false,
+        selectable: false
+    };
 
-$(editTreeSelector)
-    .tree($.extend(true, treeOptions, {selectable: true}))
-    .bind('tree.select', function (event) {
-        if (event.node) {
-            // node was selected
-            var node = event.node;
-            populateFormField(node);
+    var init = function () {
+        $(exploreTreeSelector).tree(treeOptions);
+
+        $(editTreeSelector)
+            .tree($.extend(true, treeOptions, {selectable: true}))
+            .bind('tree.select', function (event) {
+                if (event.node) {
+                    // node was selected
+                    var node = event.node;
+                    populateFormField(node);
+                }
+            })
+            .bind('tree.init', function () {
+                // Auto-open root node if it's present
+                var node = $(this).tree('getNodeById', 0);
+                $(this).tree('openNode', node);
+            });
+
+        $('#collapse-button').click(collapseTree);
+    };
+
+    var collapseTree = function () {
+        $(this).blur();
+        var $tree = $(generalTreeSelector);
+        $tree.tree('getTree')
+            .iterate(function (node) {
+                if (node.hasChildren()) {
+                    $tree.tree('closeNode', node, true);
+                }
+                return true;
+            });
+    };
+
+    var populateFormField = function (node) {
+        if ($("#parent_category")) {
+            $("#parent_category").attr("value", node.name);
+            $("#parent_category_id").attr("value", node.id);
         }
-    })
-    .bind('tree.init', function () {
-        // Auto-open root node if it's present
-        var node = $(this).tree('getNodeById', 0);
-        $(this).tree('openNode', node);
-    });
+    };
 
-var $tree = $(generalTreeSelector);
+    return {
+        init: init
+    };
 
-$('#collapse-button').click(function() {
-    $(this).blur();
-    var tree = $tree.tree('getTree');
-    tree.iterate(function (node) {
+})();
 
-        if (node.hasChildren()) {
-            $tree.tree('closeNode', node, true);
-        }
-        return true;
-    });
+$(document).ready(function () {
+    explore.init();
 });
-
-function populateFormField(node) {
-    if ($("#parent_category")) {
-        $("#parent_category").attr("value", node.name);
-        $("#parent_category_id").attr("value", node.id);
-    }
-}
-
