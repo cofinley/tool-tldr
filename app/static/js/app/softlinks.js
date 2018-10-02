d9c819e34c447e7c1ef107dda7f75bdb42c89903
Connor Finley <connor.finley5@gmail.com>
2018-10-01 20:37:22 -0400

    Get soft-links to appear on mention in FE, still need backend

---
+++ app/static/js/app/softlinks.js
@@ -0,0 +1,41 @@
+var softlinks = (function () {
+
+    var init = function () {
+        var textareas = ".sl";
+
+        $(textareas).atwho({
+            at: "!",
+            startWithSpace: false,
+            displayTpl: "<li><span class='atwho-small-text'>${type} #${id}</span>${label}</li>",
+            insertTpl: "${atwho-at}${type}-${id}",
+            data: null,
+            searchKey: "label",
+            callbacks: {
+                filter: function (q, data, searchKey) {
+                    if (null === q || q.length < 1) {
+                        return [];
+                    }
+                },
+                remoteFilter: function(q, cb) {
+                    if (null === q || q.length < 1) {
+                        return cb([]);
+                    }
+                    $.get(window.location.origin + "/search", {q: q}, function (data) {
+                        cb(data.map(function(currentValue, idx, arr){
+                            currentValue.type = currentValue.type === "c" ? "Category" : "Tool";
+                            return currentValue;
+                        }));
+                    });
+                }
+            }
+        });
+    };
+
+    return {
+        init: init
+    };
+})();
+
+$(document).ready(function () {
+    softlinks.init();
+});
